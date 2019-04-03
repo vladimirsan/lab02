4e2d6df41d0fb13544ec3534cf87659ee4f50ba6
Raven Robertson <ravewillow6383@gmail.com>
2019-04-03 14:14:03 -0700

    added 6th question to js code.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var anySiblings = prompt('Do I have any siblings?').toLowerCase();
+var anySiblings = prompt('Do I have any siblings? ').toLowerCase();
 
 if(anySiblings === 'y' || anySiblings === 'yes') {
   alert('You guessed it! I\'m the youngest of SEVEN kids!');
@@ -10,7 +10,7 @@ if(anySiblings === 'y' || anySiblings === 'yes') {
   alert('This was a yes or no question. How did you mess that up?');
 }
 
-console.log('Does the user think that I have any siblings?: ' + anySiblings);
+console.log('Does the user think that I have any siblings: ' , anySiblings);
 
 var anyPets = prompt('Do i have any pets?').toLowerCase();
 
@@ -22,7 +22,7 @@ if(anyPets === 'y' || anyPets === 'yes') {
   alert('What?? Yes or no only, please!');
 }
 
-console.log('Does the user think I have any pets? ' + anyPets);
+console.log('Does the user think I have any pets: ' , anyPets);
 
 var washingtonNative = prompt('Do you think I am native to Washington State?').toLowerCase();
 
@@ -34,7 +34,7 @@ if(washingtonNative === 'y' || washingtonNative === 'yes') {
   alert('Hmm. I think the answer you were looking for was, yes.');
 }
 
-console.log('Does the User think that I am originally from Washington State? ' + washingtonNative );
+console.log('Does the User think that I am originally from Washington State: ' , washingtonNative );
 
 var beenOnACruise = prompt('Have I ever been on a cruise?').toLowerCase();
 
@@ -46,7 +46,7 @@ if(beenOnACruise === 'y' || beenOnACruise === 'yes') {
   alert('?????');
 }
 
-console.log('Does the user think I have been on a cruise?: '+ beenOnACruise);
+console.log('Does the user think I have been on a cruise: ' , beenOnACruise);
 
 var playLotto = prompt('Do you think that I play the lottery?').toLowerCase();
 
@@ -58,8 +58,24 @@ if(playLotto === 'y' || playLotto === 'yes') {
   alert('Ehhhh, no.');
 }
 
-console.log('Does the user think that I play the lotto?' + playLotto);
+console.log('Does the user think that I play the lotto: ' , playLotto);
 
+var numberOfTattoos = Number(prompt('I will give you five chances at guessing how many tattoos I have!'));
 
+console.log('Checking prompt on first use input for how many tats they think I have:' , numberOfTattoos);
 
-
+var guessesRemaining = 5;
+// I am writing my if statement and while loop this way because it's the only format that I tried that finally worked. Although I feel as though the first if and the 2nd if should be able to be combined, I couldn't get my code to loop proberly and break the loop properly when I only had the first or third 'if' statements as opposed to both.
+if(numberOfTattoos === 6) {
+  alert('Correct! I spent more money than I care to discuss on my six tattoos!');
+} else {
+  while(guessesRemaining > 0 && numberOfTattoos !== 6) {
+    guessesRemaining--;
+    alert('Nope!');
+    numberOfTattoos = Number(prompt('Guess again! You have ' + guessesRemaining + ' more tries.'));
+    if(guessesRemaining > 0 && numberOfTattoos === 6) {
+      alert('Correct! I have spent more money than I care to discuss on my six tattoos!');
+    }
+  }
+}
+console.log('Checking user input for number of tats again:' , numberOfTattoos);
